52d3754ae7fb97acf85da32bb3e3a9683625623c
Danil <dabcom2013@gmail.com>
2025-07-15 13:58:15 +0300

    config-ms. Create config server

---
+++ services/config-ms/src/test/java/ru/cs/ex/configms/ConfigMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cs.ex.configms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
