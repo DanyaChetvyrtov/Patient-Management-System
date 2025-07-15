1543c786562ea0c90ec5d5c6146c66d08ea62957
Danil <dabcom2013@gmail.com>
2025-07-15 13:52:02 +0300

    patient-ms. Create patient ms

---
+++ services/patient-ms/src/test/java/ru/pm/ex/patientms/PatientMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pm.ex.patientms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
