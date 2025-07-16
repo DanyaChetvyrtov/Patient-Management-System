9cd0dbcc21879e7b158ecd38be45b620d5a98e79
Danil <dabcom2013@gmail.com>
2025-07-16 19:21:05 +0300

    billing-ms. Create billing-ms

---
+++ services/billing-ms/src/test/java/ru/cs/ex/billingms/BillingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cs.ex.billingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
