9a37d84326c9ee6061faa4c9b21b85152274e141
Danil <dabcom2013@gmail.com>
2025-07-17 19:46:19 +0300

    gateway-ms. Create gateway-ms

---
+++ services/gateway-ms/src/main/java/ru/cs/ex/gatewayms/GatewayMsApplication.java
@@ -0,0 +1,13 @@
+package ru.cs.ex.gatewayms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMsApplication.class, args);
+	}
+
+}
---
+++ services/gateway-ms/src/test/java/ru/cs/ex/gatewayms/GatewayMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cs.ex.gatewayms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
